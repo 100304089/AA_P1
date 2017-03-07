bcce6315fa28e399cccebc8ef4e5ff10da9dc392
10030489 <10030489@alumnos.uc3m.es>
2017-03-07 12:17:40 +0100

    pacmanCommit

---
+++ pacman/instance_generator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python busters.py -p BasicAgentAA
+python busters.py -p BasicAgentAA -l 20Hunt
+python busters.py -p BasicAgentAA -l bigHunt
+python busters.py -p BasicAgentAA -l classic
+python busters.py -p BasicAgentAA -l contestClaassic
+python busters.py -p BasicAgentAA -l newmap
+python busters.py -p BasicAgentAA -l oneHunt
+python busters.py -p BasicAgentAA -l openHunt
+python busters.py -p BasicAgentAA -l smallHunt
+python busters.py -p BasicAgentAA -l testClassic
+
