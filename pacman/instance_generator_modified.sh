6b5bda9978a7a791198add521a6d1feaf6053a3c
10030489 <10030489@alumnos.uc3m.es>
2017-03-09 13:00:19 +0100

    newMaps

---
+++ pacman/instance_generator_modified.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#!/bin/bash
+
+python busters.py -p BasicAgentAA -l 20Hunt_mod -t 0.01
+python busters.py -p BasicAgentAA -l classic_mod -t 0.01
+python busters.py -p BasicAgentAA -l bigHunt_mod -t 0.01
